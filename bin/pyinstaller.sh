d1ad698336f1f07ef0fafd74e83fc4a0e5589130
inn0kenty <innlebedev@gmail.com>
2018-03-19 19:18:50 +0300

    Add skip requirements install possibility

--- bin/pyinstaller.sh
+++ bin/pyinstaller.sh
@@ -3,6 +3,14 @@
 
 set -e
 
+# Check that install requires is needed
+if [ "$1" == "--skip-req-install" ]; then
+    skip_req_install="1"
+    shift
+else
+    skip_req_install="0"
+fi
+
 # Generate a random key for encryption
 random_key=$(pwgen -s 16 1)
 pyinstaller_args="${@/--random-key/--key $random_key}"
@@ -10,10 +18,12 @@ pyinstaller_args="${@/--random-key/--key $random_key}"
 # Use the hacked ldd to fix libc.musl-x86_64.so.1 location
 PATH="/pyinstaller:$PATH"
 
-if [ -f requirements.txt ]; then
-    pip install -r requirements.txt
-elif [ -f setup.py ]; then
-    pip install .
+if [ "$skip_req_install" == "1" ]; then
+    if [ -f requirements.txt ]; then
+        pip install -r requirements.txt
+    elif [ -f setup.py ]; then
+        pip install .
+    fi
 fi
 
 # Exclude pycrypto and PyInstaller from built packages
