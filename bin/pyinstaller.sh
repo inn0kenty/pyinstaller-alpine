fd15b5359d0e6e31e3ec18aec7a8f798669a6dbd
inn0kenty <innlebedev@gmail.com>
2018-03-20 10:54:09 +0300

    Fix skip install

--- bin/pyinstaller.sh
+++ bin/pyinstaller.sh
@@ -5,10 +5,8 @@ set -e
 
 # Check that install requires is needed
 if [ "$1" == "--skip-req-install" ]; then
-    skip_req_install="1"
+    skip_req_install=1
     shift
-else
-    skip_req_install="0"
 fi
 
 # Generate a random key for encryption
@@ -18,7 +16,7 @@ pyinstaller_args="${@/--random-key/--key $random_key}"
 # Use the hacked ldd to fix libc.musl-x86_64.so.1 location
 PATH="/pyinstaller:$PATH"
 
-if [ "$skip_req_install" == "1" ]; then
+if [ -z "$skip_req_install" ]; then
     if [ -f requirements.txt ]; then
         pip install -r requirements.txt
     elif [ -f setup.py ]; then
